a14579b00c6616c9dce907b919278ca54dfda9a9
sevillea <aseville92@gmail.com>
2023-03-08 19:35:17 +0000

    global-var

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,31 @@
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&appid=" + APIkey
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
 var APIkey = "8b5ebef5e27f399568c79c7b46e5d128"
-var searchInput = "search-input"
- var searchBtn = $("#search-button")
+$(document).ready(renderCity());
 
- searchBtn.on("click", function(){
-    searchInput = $("#search-form").val();
+// GLOBAL VARIABLES 
+const searchInput = "search-input";
+const searchBtn = $("#search-button");
+const cities =[];
+
+
+ $(searchBtn).on("click", (event) => {
+    event.preventDefault();
+    searchInput = $("#city-search").val();
+    if (searchInput === ""){
     console.log("Weather for Today is: " + searchInput);
     $.ajax({
         url: "8b5ebef5e27f399568c79c7b46e5d128",
         method:"GET"
-    }) .then(function(response){
+    }) .then(function(event){
         console.log(response);
-    })
- })
+    });
+
+    
+  
+ }});
+
+ function storeCity(cityInput) {
+    cities.push(cityInput);
+    localStorage.setItem("cities", JSON.stringify([...cities]));
+
+ }
