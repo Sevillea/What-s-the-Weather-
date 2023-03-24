683d1a806283552f628045430359f2f9f286f983
sevillea <aseville92@gmail.com>
2023-03-24 13:18:21 +0000

    JS-days/Gvar

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,31 @@
 
+$(document).ready(function (){
+    let NowMoment = moment().format("l");
+
+    // Forecast Days:
+
+    let day1 = moment().add(1, "days").format("l");
+    let day2 = moment().add(2, "days").format("l");
+    let day3 = moment().add(3, "days").format("l");
+    let day4 = moment().add(4, "days").format("l");
+    let day5 = moment().add(5, "days").format("l");
+
+    //Global Variables:
+
+    let city;
+    let cities;
+})
+
+
+
+
+
+
 $(document).ready(renderCity());
 
 // GLOBAL VARIABLES 
 const searchInput = "search-input";
 const searchBtn = $("#search-button");
-const cities =[];
 
 
  $(searchBtn).on("click", (event) => {
